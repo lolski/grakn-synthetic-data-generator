9f03a98f16756b076e1ca19422c0db738278a54d
Ganeshwara Herawan Hananda <lolski@Ganeshwaras-MacBook-Pro.local>
2018-02-21 12:41:36 +0000

    sekunda comitta

--- src/main/java/com/lolskillz/Main.java
+++ src/main/java/com/lolskillz/Main.java
@@ -13,41 +13,48 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Supplier;
 
 import static ai.grakn.graql.Graql.*;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException, ExecutionException {
+        //
+        // Parameters
+        //
         final String GRAKN_URI = "localhost:4567";
-        final String GRAKN_KEYSPACE = "grakn";
-        final int N_THREAD = 15;
-        final int N_ATTRIBUTE = 1000;
+        final String GRAKN_KEYSPACE = "grakn4";
+        final int N_THREAD = 32;
+        final int N_ATTRIBUTE = 2000;
         final ExecutorService executorService = Executors.newFixedThreadPool(N_THREAD);
 
+        //
+        // Create a schema, then perform multi-threaded data insertion where each thread inserts exactly the same data
+        //
         System.out.println("starting test. defining schema...");
         GraknSession session = Grakn.session(GRAKN_URI, GRAKN_KEYSPACE);
 
-        define(session);
-        List<CompletableFuture<Void>> asyncAll = insertAsync(executorService, session, N_ATTRIBUTE, N_THREAD);
+        CompletableFuture<Void> asyncAll = define(session)
+                .thenCompose(e -> insertAsync(executorService, session, N_ATTRIBUTE, N_THREAD));
 
-        CompletableFuture<Void> waitAll = CompletableFuture.allOf(asyncAll.toArray(new CompletableFuture[asyncAll.size()]));
-        waitAll.whenComplete((r, ex) -> {
-            System.out.println("inserting " + N_ATTRIBUTE * N_THREAD + " attribute in total of which "
+        //
+        // Cleanups: close the session and the executor service
+        //
+        asyncAll.whenComplete((r, ex) -> {
+            System.out.println("inserted " + N_ATTRIBUTE * N_THREAD + " attribute in total of which "
                     + N_ATTRIBUTE + " is unique. if post-processing works correctly, grakn should have only " + N_ATTRIBUTE + " attributes.");
+            session.close();
             try {
                 executorService.shutdown();
-                executorService.awaitTermination(300, TimeUnit.SECONDS);
+                executorService.awaitTermination(10, TimeUnit.SECONDS);
+                System.out.println("test halted");
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-            session.close();
         }).get();
     }
 
-    private static List<CompletableFuture<Void>> insertAsync(ExecutorService executorService, GraknSession session, int N_ATTRIBUTE, int N_THREAD) {
+    private static CompletableFuture<Void> insertAsync(ExecutorService executorService, GraknSession session, int N_ATTRIBUTE, int N_THREAD) {
         List<CompletableFuture<Void>> all = new LinkedList<>();
 
         for (int i = 0; i < N_THREAD; ++i) {
@@ -64,14 +71,18 @@ public class Main {
 
             all.add(future);
         }
-        return all;
+        
+        return CompletableFuture.allOf(all.toArray(new CompletableFuture[all.size()]));
     }
 
-    private static void define(GraknSession session) {
-        try (GraknTx tx = session.open(GraknTxType.WRITE)) {
-            tx.graql().define(label("value").sub("attribute").datatype(AttributeType.DataType.STRING)).execute();
-            tx.commit();
-        }
+    private static CompletableFuture<Void> define(GraknSession session) {
+        return CompletableFuture.<Void>supplyAsync(() -> {
+            try (GraknTx tx = session.open(GraknTxType.WRITE)) {
+                tx.graql().define(label("value").sub("attribute").datatype(AttributeType.DataType.STRING)).execute();
+                tx.commit();
+            }
+            return null;
+        });
     }
 
     private static void insert(GraknSession session, int executionId, int n) {
